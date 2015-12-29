efbef5a9411a10bcfe0e44f436f247788f0b1cfd
MrLukashem <mertalukasz@gmail.com>
2015-12-29 22:30:07 +0100

    Removed id for EventListener class
    Now m_idCounter id not necesery to use it.
    Also addded cont for getType method.

--- include/EventListener.h
+++ include/EventListener.h
@@ -14,28 +14,20 @@ namespace eternity {
 template <typename E>
 class EventListener {
 public:
-	using EventReceiverFn = std::function<void(E)>;
+	using EventReceiverFn = std::function<void(E&)>;
 private:
 	static_assert(std::is_base_of<E, Event>::value, TEMPLATE_PARAM_ERROR);
-
-	int m_id;
 
 	int m_type;
 
 	EventReceiverFn m_callback;
 public:
-	static int m_idCounter;
-
 	EventListener(EventReceiverFn callback) 
-	: m_id{m_idCounter++}, m_type{E::getType()}, m_callback{callback} {}
+	: m_type{E::getType()}, m_callback{callback} {}
 
 	virtual ~EventListener() {}
 
-	int inline getID() {
-		return m_id;
-	}
-
-	int inline getType() {
+	int inline getType() const {
 		return m_type;
 	}
 
@@ -47,9 +39,6 @@ public:
 		m_callback(event);
 	}
 }; //EventListener
-
-template <typename E>
-int EventListener<E>::m_idCounter = 0;
 
 } //eternity
 
